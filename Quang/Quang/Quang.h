4bf14a92323d2c015369ec799a2e4476363fef4c
quang10296 <duyquangnguyen10296@gmail.com>
2021-08-11 14:13:55 +0700

    add framework

---
+++ Quang/Quang/Quang.h
@@ -0,0 +1,18 @@
+//
+//  Quang.h
+//  Quang
+//
+//  Created by Quang Nguyễn  on 8/11/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Quang.
+FOUNDATION_EXPORT double QuangVersionNumber;
+
+//! Project version string for Quang.
+FOUNDATION_EXPORT const unsigned char QuangVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Quang/PublicHeader.h>
+
+
